a2a87cc9b0a8ac63b5c9424e872d013cf133ad61
Dominik Augustin <yeahservice@users.noreply.github.com>
2021-05-27 12:11:00 +0200

    Release 0.1.0 (#4)
    * Prepare release 0.1.0
    * Fix name of keptnsandbox dockerhub repository

--- scripts/build-push-deploy-ks3.sh
+++ scripts/build-push-deploy-ks3.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-docker build . -f Dockerfile -t keptn-sandbox/job-executor-service:latest
-docker push keptn-sandbox/job-executor-service:latest
+docker build . -f Dockerfile -t keptnsandbox/job-executor-service:latest
+docker push keptnsandbox/job-executor-service:latest
 
-docker build . -f initcontainer.Dockerfile -t keptn-sandbox/job-executor-service-initcontainer:latest
-docker push keptn-sandbox/job-executor-service-initcontainer:latest
+docker build . -f initcontainer.Dockerfile -t keptnsandbox/job-executor-service-initcontainer:latest
+docker push keptnsandbox/job-executor-service-initcontainer:latest
 
 k3s kubectl -n keptn delete deployment job-executor-service
 k3s kubectl -n keptn apply -f deploy/service.yaml -n keptn
