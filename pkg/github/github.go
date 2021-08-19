94e2551c55c7fd81969faa313ce124973da6c9fa
Dominik Augustin <dom.augustin@gmx.at>
2021-08-19 13:52:06 +0200

    Add conversion of github action inputs to kubernetes job arguments

---
+++ pkg/github/github.go
@@ -0,0 +1,39 @@
+package github
+
+import (
+	"fmt"
+	"keptn-sandbox/job-executor-service/pkg/github/model"
+	"log"
+	"strings"
+)
+
+func prepareArgs(with map[string]string, inputs map[string]model.Input, args []string) ([]string, error) {
+	var filledArgs []string
+
+	for inputKey, inputValue := range inputs {
+		argKey := fmt.Sprintf("inputs.%s", inputKey)
+		log.Printf("argKey: %v", argKey)
+
+		for _, arg := range args {
+			if strings.Contains(arg, argKey) {
+				log.Printf("matched argKey: %v", argKey)
+
+				argValue := inputValue.Default
+				if withValue, ok := with[inputKey]; ok {
+					argValue = withValue
+				} else {
+					if inputValue.Required {
+						return nil, fmt.Errorf("required input '%s' not provided", inputKey)
+					}
+				}
+
+				splittedArg := strings.Split(arg, "$")
+				arg := strings.TrimSpace(splittedArg[0])
+				filledArgs = append(filledArgs, arg)
+				filledArgs = append(filledArgs, argValue)
+			}
+		}
+	}
+
+	return filledArgs, nil
+}
---
+++ pkg/github/github_test.go
@@ -0,0 +1,43 @@
+package github
+
+import (
+	"gotest.tools/assert"
+	"keptn-sandbox/job-executor-service/pkg/github/model"
+	"log"
+	"testing"
+)
+
+func TestPrepareArgs(t *testing.T) {
+	with := map[string]string{"scan-type": "banana", "format": "cucumber"}
+	inputs := map[string]model.Input{
+		"scan-type": {
+			Required: false,
+			Default:  "",
+		},
+		"format": {
+			Required: false,
+			Default:  "",
+		},
+		"template": {
+			Required: false,
+			Default:  "table",
+		},
+	}
+	args := []string{"-a ${{ inputs.scan-type }}", "-b ${{ inputs.format }}", "-c ${{ inputs.template }}"}
+	k8sArgs, err := prepareArgs(with, inputs, args)
+	assert.NilError(t, err)
+	log.Printf("%v", k8sArgs)
+}
+
+func TestPrepareArgs_RequiredInput(t *testing.T) {
+	with := map[string]string{}
+	inputs := map[string]model.Input{
+		"scan-type": {
+			Required: true,
+			Default:  "",
+		},
+	}
+	args := []string{"-a ${{ inputs.scan-type }}"}
+	_, err := prepareArgs(with, inputs, args)
+	assert.Error(t, err, "required input 'scan-type' not provided")
+}
