9a84aed330152421f3fb9c4fe4adefaa20a83ff3
TannerGabriel <gabrieltanner.code@gmail.com>
2021-11-26 20:38:37 +0100

    TTLSecondsAfterFinished default value
    Signed-off-by: TannerGabriel <gabrieltanner.code@gmail.com>

--- pkg/k8sutils/job.go
+++ pkg/k8sutils/job.go
@@ -79,6 +79,13 @@ func (k8s *k8sImpl) CreateK8sJob(jobName string, action *config.Action, task con
 		return fmt.Errorf("could not prepare env for job %v: %v", jobName, err.Error())
 	}
 
+	var TTLSecondsAfterFinished *int32
+	if task.TTLSecondsAfterFinished == nil {
+		*TTLSecondsAfterFinished = 600
+	} else {
+		TTLSecondsAfterFinished = task.TTLSecondsAfterFinished
+	}
+
 	jobSpec := &batchv1.Job{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      jobName,
@@ -168,7 +175,7 @@ func (k8s *k8sImpl) CreateK8sJob(jobName string, action *config.Action, task con
 				},
 			},
 			BackoffLimit: &backOffLimit,
-			TTLSecondsAfterFinished: task.TTLSecondsAfterFinished,
+			TTLSecondsAfterFinished: TTLSecondsAfterFinished,
 		},
 	}
 
