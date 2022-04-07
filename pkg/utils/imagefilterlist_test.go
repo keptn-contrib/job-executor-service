a668928e661a110970674513141f88ab5f949f9c
Raphael Ludwig <raphael.ludwig@dynatrace.com>
2022-04-07 09:42:19 +0200

    tests: Split some tests for image filter list
    Signed-off-by: Raphael Ludwig <raphael.ludwig@dynatrace.com>

--- pkg/utils/imagefilterlist_test.go
+++ pkg/utils/imagefilterlist_test.go
@@ -30,24 +30,28 @@ func TestSpecificImagesMustBeAccepted(t *testing.T) {
 }
 
 func TestDefaultRegistry(t *testing.T) {
-	allowedImageList := []string{"{docker.io/[AB]/some-image,some-image}", "my-user/my-image:1.2.3"}
+	allowedImageList := []string{"docker.io/[AB]/some-image", "docker.io/my-user/my-image:1.2.3"}
 	helper, err := NewImageFilterList(allowedImageList)
 	require.NoError(t, err)
 
-	assert.True(t, helper.Contains("some-image"))
+	assert.True(t, helper.Contains("docker.io/A/some-image"))
+	assert.True(t, helper.Contains("docker.io/B/some-image"))
+	assert.True(t, helper.Contains("docker.io/my-user/my-image:1.2.3"))
+	assert.False(t, helper.Contains("docker.io/my-user/some-image"))
+
+	// Do not allow images without a registry
 	assert.False(t, helper.Contains("B/some-image"))
 	assert.False(t, helper.Contains("some-image:123"))
 	assert.False(t, helper.Contains("B/some-image:123"))
-	assert.True(t, helper.Contains("docker.io/A/some-image"))
-	assert.True(t, helper.Contains("docker.io/B/some-image"))
-	assert.False(t, helper.Contains("docker.io/my-user/some-image"))
-	assert.False(t, helper.Contains("docker.io/my-user/my-image:1.2.3"))
+	assert.False(t, helper.Contains("my-user/some-image"))
+	assert.False(t, helper.Contains("my-user/my-image:1.2.3"))
 
+	// Do not allow images from other registries
 	assert.False(t, helper.Contains("ghcr.io/my-user/some-image"))
 	assert.False(t, helper.Contains("ghcr.io/my-user/my-image:latest"))
 }
 
-func TestAcceptEverythingFromDefaultRegistry(t *testing.T) {
+func TestWildcardFormatsRegistry(t *testing.T) {
 	allowedImageList := []string{"docker.io/*", "*[AB]/some-image*"}
 	helper, err := NewImageFilterList(allowedImageList)
 	require.NoError(t, err)
@@ -55,27 +59,38 @@ func TestAcceptEverythingFromDefaultRegistry(t *testing.T) {
 	assert.True(t, helper.Contains("docker.io/A/some-image"))
 	assert.True(t, helper.Contains("B/some-image"))
 	assert.True(t, helper.Contains("A/some-image:latest"))
-	assert.False(t, helper.Contains("some-image"))
 	assert.True(t, helper.Contains("docker.io/my-user/my-image:1.2.3"))
+	assert.True(t, helper.Contains("ghcr.io/B/some-image"))
 
+	assert.False(t, helper.Contains("some-image"))
+	assert.False(t, helper.Contains("ghcr.io/A/my-image:1.2.3"))
 	assert.False(t, helper.Contains("ghcr.io/my-user/some-image"))
 	assert.False(t, helper.Contains("ghcr.io/my-user/my-image:latest"))
 }
 
-func TestVariousImageFormats(t *testing.T) {
-	allowedImageList := []string{"image", "docker.io/*", "ghcr.io/*", "a.b.c.d.e.f.g.registry/*", "a.registry:1337/*", "{user/image@sha256*,image@sha256*}"}
+func TestDifferentContainerRegistryFormats(t *testing.T) {
+	allowedImageList := []string{"ghcr.io/*", "a.b.c.d.e.f.g.registry/*", "a.registry:1337/*"}
+	helper, err := NewImageFilterList(allowedImageList)
+	require.NoError(t, err)
+
+	assert.True(t, helper.Contains("a.b.c.d.e.f.g.registry/user/a"))
+	assert.True(t, helper.Contains("a.registry:1337/user/a"))
+
+	assert.False(t, helper.Contains("a.b.c.d.e.f.g.registry:8080/user/a"))
+	assert.False(t, helper.Contains("a.registry:1338/user/a"))
+	assert.False(t, helper.Contains("docker.io/user/a"))
+}
+
+func TestImageHashFormats(t *testing.T) {
+	allowedImageList := []string{"ghcr.io/user/image@sha256*", "image@sha256:a428de4495b23ak*"}
 	helper, err := NewImageFilterList(allowedImageList)
 	require.NoError(t, err)
 
 	assert.True(t, helper.Contains("image@sha256:a428de4495b23ak3402k37a5881c2d2cffa93757d34996156e4ea544577ab7f3"))
-	assert.True(t, helper.Contains("user/image@sha256:a42bdea4a4059631459237a5831224f4ffa93757d99056156efea544577ab7f3"))
 	assert.True(t, helper.Contains("ghcr.io/user/image@sha256:a428de44a9059fb1a59237a5881c2ddcffa93757d94026156e5ea544577ab3f3"))
-	assert.True(t, helper.Contains("a.b.c.d.e.f.g.registry/user/a"))
-	assert.True(t, helper.Contains("a.registry:1337/user/a"))
 
+	assert.False(t, helper.Contains("user/image@sha256:a42bdea4a4059631459237a5831224f4ffa93757d99056156efea544577ab7f3"))
 	assert.False(t, helper.Contains("forbidden.registry/abc/image@sha256:4428de44a9059fb1a49237a5881c2d2cffa9375749902e156ff3a544577ab7f3"))
-	assert.False(t, helper.Contains("a.b.c.d.e.f.g.registry:8080/user/a"))
-	assert.False(t, helper.Contains("a.registry:1338/user/a"))
 }
 
 func TestBuildImageAllowListFunction(t *testing.T) {
