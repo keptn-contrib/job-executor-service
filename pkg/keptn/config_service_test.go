c301f07d654a05dd4816a7f8acc73d44914e4573
Paolo Chila <paolo.chila@dynatrace.com>
2022-04-14 08:12:29 +0200

    test: add unit tests for file package
    Signed-off-by: Paolo Chila <paolo.chila@dynatrace.com>

--- pkg/keptn/config_service_test.go
+++ pkg/keptn/config_service_test.go
@@ -2,6 +2,7 @@ package keptn
 
 import (
 	"net/url"
+	"path"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -78,4 +79,77 @@ func TestGetAllKeptnResources(t *testing.T) {
 	val, ok = keptnResources[locustFunctional]
 	assert.True(t, ok)
 	assert.Equal(t, string(val), locustFunctional)
+}
+
+func TestGetAllKeptnResourcesLocal(t *testing.T) {
+	locustPath := "/locust"
+	locustBasic := path.Join(locustPath, "basic.py")
+	locustFunctional := path.Join(locustPath, "functional.py")
+
+	resourceHandlerMock := CreateResourceHandlerMock(t)
+	resourceHandlerMock.EXPECT().GetAllServiceResources(project, stage, service).Times(0)
+
+	resourceHandlerMock.EXPECT().GetServiceResource(
+		project, stage, service, url.QueryEscape(locustBasic),
+	).Times(0)
+
+	resourceHandlerMock.EXPECT().GetServiceResource(
+		project, stage, service, url.QueryEscape(locustFunctional),
+	).Times(0)
+
+	configService := NewConfigService(true, project, stage, service, resourceHandlerMock)
+	fs := afero.NewMemMapFs()
+
+	err := createFile(fs, locustBasic, []byte(locustBasic))
+	require.NoError(t, err)
+
+	err = createFile(fs, locustFunctional, []byte(locustFunctional))
+	require.NoError(t, err)
+
+	keptnResources, err := configService.GetAllKeptnResources(fs, locustPath)
+	require.NoError(t, err)
+
+	val, ok := keptnResources[locustBasic]
+	assert.True(t, ok)
+	assert.Equal(t, locustBasic, string(val))
+
+	val, ok = keptnResources[locustFunctional]
+	assert.True(t, ok)
+	assert.Equal(t, string(val), locustFunctional)
+}
+
+func TestErrorNoDirectoryResourcesLocal(t *testing.T) {
+	locustPath := "/locust"
+	locustBasic := path.Join(locustPath, "basic.py")
+	locustFunctional := path.Join(locustPath, "functional.py")
+
+	resourceHandlerMock := CreateResourceHandlerMock(t)
+	resourceHandlerMock.EXPECT().GetAllServiceResources(project, stage, service).Times(0)
+
+	resourceHandlerMock.EXPECT().GetServiceResource(
+		project, stage, service, url.QueryEscape(locustBasic),
+	).Times(0)
+
+	resourceHandlerMock.EXPECT().GetServiceResource(
+		project, stage, service, url.QueryEscape(locustFunctional),
+	).Times(0)
+
+	configService := NewConfigService(true, project, stage, service, resourceHandlerMock)
+	fs := afero.NewMemMapFs()
+
+	_, err := configService.GetAllKeptnResources(fs, locustPath)
+	require.Error(t, err)
+}
+
+func createFile(fs afero.Fs, fileName string, content []byte) error {
+	file, err := fs.Create(fileName)
+	if err != nil {
+		return err
+	}
+	file.Write(content)
+	err = file.Close()
+	if err != nil {
+		return err
+	}
+	return nil
 }
