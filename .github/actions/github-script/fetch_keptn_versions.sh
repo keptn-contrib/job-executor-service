c916a8d3a629c5c7615833bfe06273fed114b9e3
TannerGabriel <gabrieltanner.code@gmail.com>
2022-07-11 11:51:15 +0200

    Check Helm chart repository
    Signed-off-by: TannerGabriel <gabrieltanner.code@gmail.com>

--- .github/actions/github-script/fetch_keptn_versions.sh
+++ .github/actions/github-script/fetch_keptn_versions.sh
@@ -9,15 +9,5 @@ echo $release
 
 matrix_config=""
 
-# Check if release or prerelease are empty
-if [[ -n "$release" && -n "$prerelease" ]]
-then
-    # Build a matrix JSON string
-    matrix_config=$(echo "{\"keptn-version\":[\"$prerelease\", \"$release\"]}")
-    echo "::set-output name=KEPTN_MATRIX::$matrix_config"
-else
-    echo "Release or Prerelease not found"
-    echo "::set-output name=KEPTN_MATRIX::{}"
-fi
-
-
+echo "::set-output name=LATEST_RELEASE::$release"
+echo "::set-output name=LATEST_PRERELEASE::$prerelease"
