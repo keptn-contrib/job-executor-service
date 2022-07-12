fac35db6251619d709fd9ebd8fedcd98cd6fd412
TannerGabriel <gabrieltanner.code@gmail.com>
2022-07-12 10:24:35 +0200

    Sort before picking release version
    Signed-off-by: TannerGabriel <gabrieltanner.code@gmail.com>

--- .github/actions/github-script/fetch_keptn_versions.sh
+++ .github/actions/github-script/fetch_keptn_versions.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # Fetch latest release and prerelease
-prerelease=$(curl -s https://api.github.com/repos/keptn/keptn/releases | jq -r 'map(select(.prerelease)) | first | .tag_name')
-release=$(curl -s https://api.github.com/repos/keptn/keptn/releases | jq -r 'map(select(.prerelease==false)) | first | .tag_name')
-
-matrix_config=""
+prerelease=$(curl -s https://api.github.com/repos/keptn/keptn/releases | jq -r 'map(select(.prerelease)) | sort_by(.tag_name)[-1].tag_name')
+release=$(curl --silent "https://api.github.com/repos/keptn/keptn/releases/latest" | jq -r '.tag_name')
 
 # Write variables as output
 echo "::set-output name=LATEST_RELEASE::$release"
